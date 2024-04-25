c9784c8880748fda8d32d5fb1d82eb8a0299344b
rupeshmanikanta <61379479+rupeshmanikanta@users.noreply.github.com>
2024-04-25 17:09:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
+from flask import Flask
 
+app = Flask(name)
+
+@app.route('/') def home(): return 'Hello, World! This is my sample web application.'
+
+if name == 'main': app.run(host='0.0.0.0', port=5000)
